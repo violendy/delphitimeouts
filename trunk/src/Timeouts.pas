ba9b622fee0d1e0a70de43c29096ad44a7e65513
sjachim <sjachim@9881a654-cce4-11dd-a2b5-e5f827957e07>
2008-12-18 09:29:31 +0000

    initial import

--- trunk/src/Timeouts.pas
+++ trunk/src/Timeouts.pas
@@ -25,26 +25,26 @@ unit Timeouts;
 
 
 {
-Let's you limit execution time of a procedure/function. Timeout will be called off automatically with the execution of a last statement of the procedure/function in which it was created. The thread which didn't finish it's work, won't be killed abruptly! It will magically raise ETimeoutException, so you have the ability to release resources and free memory during stack unwinding... And you can use it from GUI/main thread.
+Let's you limit execution time of a procedure/function. 
+Timeout will be called off automatically with the execution 
+of a last statement of the procedure/function in which it was created. 
+The thread which didn't finish it's work, won't be killed abruptly! 
+It will magically raise ETimeoutException, so you have the ability to release 
+resources and free memory during stack unwinding... 
+And you can use it from GUI/main thread.
 
-procedure TForm1.Button1Click?(Sender: TObject); begin
-
-    try
-
-        timeout(4000); // this is where "magic" happens... while True do begin
-
-            Sleep(1000); // some real processing here
-
-        end;
-
-    except
-
-        on E: Exception do
-
-            ShowMessage?(E.ClassName?);
-
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  try
+    timeout(4000); // this is where "magic" happens...
+    while True do
+    begin
+      Sleep(10000); // some real processing here
     end;
-
+  except
+    on E: Exception do
+      ShowMessage(E.ClassName);
+  end;
 end;
 }
 
